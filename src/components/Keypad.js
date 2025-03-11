539c5b66c3159ea02fb46921bae2aa0e18a5a0dc
FranklinMburuDev <184284364+FranklinMburu@users.noreply.github.com>
2025-03-12 00:30:11 +0300

    added event handler

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
-// Code Keypad Component Here
-
-function Keypad (){
+function Keypad() {
+    // Event handler for the onChange event
+    const handleChange = () => {
+      console.log('Entering password...');
+    };
+  
     return (
-        <div></div>
-    )
-}
-
-export default Keypad;
+      <div>
+        {/* Add an input field of type "password" and attach the onChange event handler */}
+        <input type="password" onChange={handleChange} />
+      </div>
+    );
+  }
+  
+  export default Keypad;
